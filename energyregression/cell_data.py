5830bd2b570650f9b27b5a7aa5fac9132937e442
bs544 <cdt1804@cdt18-laptop-04.lsc.phy.private.cam.ac.uk>
2019-08-01 14:27:39 +0100

    Adding the energy regression to the spin regression repo, starting with cas_data.py

---
+++ energyregression/cell_data.py
@@ -0,0 +1,300 @@
+import numpy as np
+from parsers.dft.parser_castep import parse
+#parser code courtesy of Andrew Fowler
+from parsers.structure_class import supercell
+import pickle
+import os
+import copy
+import time
+
+class Cell_data():
+    def __init__(self,datadir,savedir,verbose=False,savename='cell_data'):
+        self.datadir = datadir
+        self.savedir = savedir
+        self.verbose = verbose
+        self.savename = savename
+        self.data = []
+        self.keys = ['cell','positions','forces','energy','spins']
+
+    def get_spins_from_castep(self,filename,natoms):
+        """
+        Parameters:
+            filename: (str) name of the .castep file to examine
+            natoms: (int) number of atoms, this has to be the same as the length of the array
+        Returns:
+            data: (array) shape (N,) Mulliken Analysis of spin contribution from each atom in units of hbar/2
+        """
+        assert (filename[-7:]==".castep"), "Incorrect file type submitted to get_spins_from_castep, needs to be .castep, not {}".format(filename[-7:])
+
+        with open(filename,'r') as f:
+            lines = f.readlines()
+        
+        spin_calc = True
+
+        for line in lines:
+            if ('treating system as non-spin-polarized' in line):
+                spin_calc = False
+                break
+        if (not spin_calc):
+            if (self.verbose):
+                print('Spin independent calculations, returning None as spin')
+            return None
+
+        data = []
+        for line in lines:
+            if ('  Fe              ' in line):
+                data.append(float(line.split()[-1]))
+        data = np.asarray(data)
+        assert (len(data)==natoms), "data isn't the right size"
+        return data
+    
+    def load_castep(self,filename):
+        """
+        Parameters:
+            file: (str) name of the .castep file to load
+        Returns:
+            data: (dict) dictionary containing the following keys:
+                    'energy': energy of the cell in eV
+                    'cell': (3,3) array of cell vectors in Angstroms
+                    'forces': (N,3) array of forces in eV/Angstrom
+                    'positions': (N,3) array of positions in Angstroms
+                    'spins': (N,) array of Mulliken spins (hbar/2)
+            status: (bool) if it returns false, then the load has failed
+        """
+
+        if ('/' not in filename):
+            filename = '{}{}'.format(self.datadir,filename)
+            if (self.verbose):
+                print('No directory specified, assuming the desired file is in {}'.format(self.datadir))
+
+        
+        ftype = 'castep'
+        l = len(ftype)
+
+        try:
+            assert ftype in filename[-l:], 'need to pass a .castep file to the get_positions() function of the parser'
+        except AssertionError as error:
+            if (self.verbose):
+                print(error)
+            return None, False
+        data = {}
+
+        cas_parser = parse(filename,ftype)
+        cas_parser.run()
+        cas_cell = cas_parser.get_supercells()[0]
+
+        posns = cas_cell.get_positions()
+        species = cas_cell.get_species()
+        cell = cas_cell.get_cell()
+        forces = cas_cell.get_forces()
+        energy = cas_cell.get_energy()
+
+        #positions are fractional coordinates when they come out
+        #multiply by cell values to get coordinates in Angstroms
+        for i in range(posns.shape[0]):
+            posns[i,:] = np.dot(cell,posns[i,:])
+
+        data['cell'] = cell
+        data['elements'] = species
+        data['positions'] = posns
+        data['forces'] = forces
+        data['energy'] = energy
+
+        spins = self.get_spins_from_castep(filename,posns.shape[0])
+        data['spin'] = spins
+ 
+        return data, True
+    
+    def load_castep_dir(self,dir_=None,number=None):
+        """
+        Parameters
+            dir: (str) if this isn't none, then this is used as the directory from which .castep files are parsed
+            number: (int) number of files to load, 
+                if this is None or the number of files in the directory is less than this number, the all the files are considered
+        Returns:
+            datalist: (list) each element is a dictionary with the following keys: 'energy','cell','forces','spin','positions'
+            status: (bool) whether any useable data was loaded
+        """
+
+        if (dir_ is not None):
+            datadir = dir_
+        else:
+            datadir = self.datadir
+
+        assert os.path.isdir(datadir), "Specified directory: {} is not a directory".format(datadir)
+        
+        files = sorted(os.listdir(datadir))
+
+        if (number is not None):
+            num_files = number
+        else:
+            num_files = len(files)
+        
+        datalist = []
+        cntr = 0
+
+
+        for file_ in files:
+            filename = '{}{}'.format(datadir,file_)
+            data, goodload = self.load_castep(filename)
+            if (goodload):
+                cntr += 1
+                datalist.append(data)
+                if (cntr == num_files):
+                    if (self.verbose):
+                        print('{} files loaded'.format(cntr))
+                    return datalist, True
+        
+        if (self.verbose):
+            print('{} files loaded out of a possible {}'.format(cntr,len(files)))
+        
+        if (len(datalist)>0):
+            status = True
+        else:
+            status = False
+
+        return datalist, status
+
+    def load_pickle_dir(self,pickle_dir=None):
+        """
+        Parameters:
+            pickle_dir: (str) directory of pickle files, if None then use self.savedir
+        Returns:
+            datalist: (list) each element is a dictionary with the following keys: 'energy','cell','forces','spin','positions'
+                    These are loaded from the .pckl files, which is quicker than getting them from the .castep files
+            status: (bool) if the load goes well
+        """
+        
+        if (pickle_dir is None):
+            pickle_dir = self.savedir
+        
+        try:
+        
+            assert os.path.isdir(pickle_dir), "{} is not a directory".format(pickle_dir)
+        except AssertionError as error:
+            if (self.verbose):
+                print(error)
+            return None, False
+        
+        files = sorted(os.listdir(pickle_dir))
+
+        datalist = []
+
+        for file_ in files:
+            if (".pckl" not in file_):
+                continue
+            filename = '{}{}'.format(pickle_dir,file_)
+
+            with open(filename,'rb') as f:
+                saved_dict = pickle.load(f)
+
+                try:
+                    assert all(key in saved_dict.keys() for key in self.keys) and \
+                        all(key in self.keys for key in saved_dict.keys()), "Saved dictionary has an incompatible set of keys"
+                except AssertionError as error:
+                    if (self.verbose):
+                        print(error)
+                        print('Skipping file {}'.format(file_))
+                    continue
+            
+            datalist.append(saved_dict)
+        
+        if (len(datalist)> 0):
+            if (self.verbose):
+                print('{} files loaded out of a possible {} in the directory'.format(len(datalist),len(files)))
+            return datalist, True
+        else:
+            if (self.verbose):
+                print('No files loaded')
+            return datalist, False
+    
+    def save_data(self,datalist,savedir=None,savename=None):
+        """
+        Parameters:
+            datalist: (list) elements are dictionaries to be saved
+            savedir: (str) directory to which the .pckl files are saved, if None then self.savedir is used
+            savename: (str) base name given to the .pckl files, if None then self.savename is used
+        Actions:
+            saves the dictionaries in the list as individual pickle files
+        Returns:
+            status: (bool) whether the save was good
+        """
+        
+        if (savedir is None):
+            savedir = self.savedir
+        
+        if (savename is None):
+            savename = self.savename
+        
+        if (not os.path.isdir(savedir)):
+            os.mkdir(savedir)
+            if (self.verbose):
+                print("{} doesn't exist, creating directory".format(savedir))
+        
+        for i, data in enumerate(datalist):
+            filename = '{}{}_{}.pckl'.format(savedir,savename,i)
+
+            if (self.verbose):
+                print("Saving castep data into {}".format(filename))
+            
+            with open(filename,'wb') as f:
+                pickle.dump(data,f)
+        return True
+    
+    def load_cell_data(self,load=True,save=True,datadir=None,savedir=None,cell_list=None):
+        """
+        Parameters:
+            load: (bool) attempt to load existing pickle data if True
+            save: (bool) if no pickle data exists, save the parsed data in this form if True
+            datadir: (str) The directory from which .castep files are parsed, if None then self.datadir is used
+            savedir: (str) directory to which the .pckl files are saved, if None then self.savedir is used
+            cell_list: (list) contains list of data indices to use
+                      (int)  contains number of files to parse
+                      If None, then parse all of the files
+        Actions:
+            loads all of the data specified
+        """
+        start = time.time()
+
+        if (savedir is None):
+            savedir = self.savedir
+        if (datadir is None):
+            datadir = self.datadir
+
+        if (load):
+            if (self.verbose):
+                print('Attempting to load data')
+            datalist,goodload = self.load_pickle_dir(pickle_dir=savedir)
+            if (goodload):
+                if (self.verbose):
+                    print('Good Load')
+                self.data = datalist
+                return
+            elif (self.verbose):
+                print('Failed to load, will read data from {} instead'.format(datadir))
+        
+        datalist,goodload = self.load_castep_dir(dir_=datadir)
+
+        if (goodload):
+            if (self.verbose):
+                print('Data parsed successfully')
+            self.data = datalist
+        elif (not goodload and self.verbose):
+            print('Data not parsed successfully, Aborting')
+            return
+        
+        if (save):
+            well_saved = self.save_data(self.data,savedir=savedir)
+            if (well_saved and self.verbose):
+                print('Data saved successfully')
+            elif (not well_saved and self.verbose):
+                print('Data not saved successfully')
+        
+        if (self.verbose):
+            print('Time taken: {0:.2f}s'.format(time.time()-start))
+        return
+            
+
+
+
+
